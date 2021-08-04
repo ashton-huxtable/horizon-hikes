6654b63d1fa27a9396503560a33a2bd522702639
Ashton Huxtable <aehuxtable@gmail.com>
2021-08-04 13:37:56 -0600

    Add USA map and state picker component to pass state name for filtering

---
+++ src/components/StatePicker/StatePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import USAMap from 'react-usa-map';
+
+const StatePicker = (props) => {
+
+  const mapHandler = (event) => {
+    props.selectState(event.target.dataset.name)
+    alert(event.target.dataset.name);
+  };
+  return(
+    <USAMap onClick={mapHandler}/>
+  )
+}
+
+export default StatePicker;
