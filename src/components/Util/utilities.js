6fa4d31e4e8debc9a28f3ef4168a7f10ba715815
Ashton Huxtable <aehuxtable@gmail.com>
2021-08-05 13:54:32 -0600

    Create cleaning function for park details data

--- src/components/Util/utilities.js
+++ src/components/Util/utilities.js
@@ -1,5 +1,34 @@
-export const filterParksForHiking = (parksData) => {
+const filterParksForHiking = (parksData) => {
   const parksWithHiking = parksData.data.find(activity => activity.name === 'Hiking')
 
   return parksWithHiking
 }
+
+const filterParkDetails = (parkDetailsData) => {
+  console.log(parkDetailsData, 'util')
+
+  const {
+    id,
+    fullName,
+    description,
+    latitude,
+    longitude,
+    activities,
+    addresses,
+    images,
+    } = parkDetailsData.data[0]
+
+  const filteredDetails = {
+      'id': id,
+      'fullName': fullName,
+      'description': description,
+      'latitude': latitude,
+      'longitude': longitude,
+      'activities': activities,
+      'addresses': addresses,
+      'images': images,
+  }
+  return filteredDetails
+}
+
+export { filterParksForHiking, filterParkDetails }
