7ac857352e5f333f7d9d513e3d98a2f4b6098a72
Ashton Huxtable <aehuxtable@gmail.com>
2021-08-06 14:26:08 -0600

    Add button to go back to parks list and pass activities down as props

--- src/components/ParkDetails/ParkDetails.js
+++ src/components/ParkDetails/ParkDetails.js
@@ -1,8 +1,11 @@
 import { filterParkDetails } from '../Util/utilities';
 import { getParkDetailsData } from '../Util/apiCalls';
+import { Link } from 'react-router-dom';
 import React, { useState, useEffect }from 'react';
 import InfoCard from '../InfoCard/InfoCard';
 import SunriseSunset from '../SunriseSunset/SunriseSunset';
+import ParkActivities from '../ParkActivities/ParkActivities';
+
 
 
 const ParkDetails = ({selectedPark}) => {
@@ -21,14 +24,29 @@ const ParkDetails = ({selectedPark}) => {
     getDetails()
   }, [])
 
+  console.log(parkDetails.activities)
+
+
     return(
       <main className='park-details'>
+        <Link to='/parksByState'>
+          <button>go back</button>
+
+        </Link>
         {
           !Object.keys(parkDetails).length ?
           <h2>loading......</h2> :
          <InfoCard name={parkDetails.fullName} address={parkDetails.address} description={parkDetails.description}/>
         }
-        <SunriseSunset />
+        <SunriseSunset latitude={parkDetails.latitude} longitude={parkDetails.longitude}/>
+
+        <section className='activities'>
+          {
+            !Object.keys(parkDetails).length ?
+            <h2>loading activities.... </h2> :
+          < ParkActivities activities={parkDetails.activities}/>
+          }
+        </section>
       </main>
     )
   }
