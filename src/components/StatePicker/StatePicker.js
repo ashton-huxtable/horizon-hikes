45e256a5a64038252005cd236bde03367fd2c244
Ashton Huxtable <aehuxtable@gmail.com>
2021-08-07 14:51:36 -0600

    Add basic styling to map

--- src/components/StatePicker/StatePicker.js
+++ src/components/StatePicker/StatePicker.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import React from 'react';
 import USAMap from 'react-usa-map';
+import './StatePicker.css';
 
 const StatePicker = (props) => {
 
@@ -10,9 +11,12 @@ const StatePicker = (props) => {
   }
 
   return(
-    <Link to='/parksByState'>
-      <USAMap onClick={mapHandler}/>
-    </Link>
+    <main>
+      <h2 className='choose-state'>Choose a State Below to Plan Your Trip! </h2>
+      <Link to='/parksByState'>
+        <USAMap onClick={mapHandler}/>
+      </Link>
+    </main>
   )
 }
 
