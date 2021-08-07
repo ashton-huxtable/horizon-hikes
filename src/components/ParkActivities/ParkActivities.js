54715e7d497c9a9e41629bf30c64b7a3d2818e9a
Ashton Huxtable <aehuxtable@gmail.com>
2021-08-06 18:56:55 -0600

    Add key to activities

--- src/components/ParkActivities/ParkActivities.js
+++ src/components/ParkActivities/ParkActivities.js
@@ -3,12 +3,12 @@ import React from 'react';
 const ParkActivities = ({activities}) => {
   const moreActivites = activities.map(activity => {
     return(
-      <p>{activity.name}</p>
+      <p key={activity.id}>{activity.name}</p>
     )
   })
 
   return(
-    <p className='activities'>{moreActivites}</p>
+    <section className='activities'>{moreActivites}</section>
   )
 }
 
