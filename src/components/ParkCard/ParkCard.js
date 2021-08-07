1ad867a6bc2b523fefee68e8d11094ae8fca8bcc
Ashton Huxtable <aehuxtable@gmail.com>
2021-08-07 17:09:38 -0600

    Add styling for park card and list

--- src/components/ParkCard/ParkCard.js
+++ src/components/ParkCard/ParkCard.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './ParkCard.css';
+import parkPhoto from '../../parkCardImg.jpg'
 
 const ParkCard = ({name, designation, id}) => {
 
 
   return(
     <article className='park-card'>
-      <h3>{name}</h3>
-      <p>{designation}</p>
+      <img className='park-card-img' src={parkPhoto} alt='trees and mountains' />
+      <div className='name-des'>
+        <h3 className='park-card-name'>{name}</h3>
+        <p className='park-card-des'>{designation}</p>
+      </div>
       <Link to={`/details/${id}`}>
-        <button>More Info</button>
+        <button className='more-info'>Details</button>
       </Link>
     </article>
   )
