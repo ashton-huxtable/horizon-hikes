975ae10ede61fbb9f2ebf00396c8643d2d2a0f60
Ashton Huxtable <aehuxtable@gmail.com>
2021-08-06 20:00:03 -0600

    Map over future trips and pass to trip card to be rendered

--- src/components/FutureVisits/FutureVisits.js
+++ src/components/FutureVisits/FutureVisits.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import TripCard from '../TripCard/TripCard';
 
 const FutureVisits = ({futureTrips}) => {
 
+  const userTrips = futureTrips.map(trip => {
+      return(
+        <TripCard 
+          image={trip.image}
+          name={trip.fullName}
+          date={trip.tripDate.tripDate}
+          sunrise={trip.sunRiseSet.sunrise}
+          sunset={trip.sunRiseSet.sunset}
+          id={trip.id}
+          key={trip.id}
+        />
+      )
+  })
 
-  
+  return(
+    <section className='future-trips'>
+      {userTrips}
+    </section>
+  )
 }
 
 export default FutureVisits;
